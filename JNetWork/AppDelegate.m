457b3526bfde7f24a713bd079a68872c1493a770
jackytianhappy <1142899950@qq.com>
2016-10-15 16:25:59 +0800

    提交更改

--- JNetWork/AppDelegate.m
+++ JNetWork/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    ViewController *root = [[ViewController alloc]init];
+    self.window.rootViewController = root;
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
